1c18666d1e7ad8c09b1d32ebf0e96942fcc51c3d
zbl14 <ifthereisoneday@gmail.com>
2022-07-26 08:58:24 -0700

    set property for Animal

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -2,6 +2,11 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
-    // properties, constructors, methods, etc. go here
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public DateTime Date { get; set; }
+    public string Breed { get; set; }
   }
 }
